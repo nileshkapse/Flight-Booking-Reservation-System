d0acd1e438a8ce915c3daa5ec417579efab39a5b
nileshkapse <kapsenileshp18@gmail.com>
2022-04-13 15:20:21 +0530

    UI Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FlightBookingComponent } from './DashBoard/flight-booking/flight-booking.component';
+import { InvoiceComponent } from './DashBoard/invoice/invoice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'flight-booking', component: FlightBookingComponent},
+  { path: 'invoice', component: InvoiceComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlightBookingComponent } from './DashBoard/flight-booking/flight-booking.component';
+import { InvoiceComponent } from './DashBoard/invoice/invoice.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightBookingComponent,
+    InvoiceComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
