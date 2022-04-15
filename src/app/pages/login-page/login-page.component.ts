c29a56b4bb5898a37392159cb5f1501e432bac75
Sanket Salunke <salunkess2001@gmail.com>
2022-04-15 11:09:53 +0530

    1) Reset feature added to Cancel button in Signup form 2) Login form data accessed into .ts file

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+  username: string;
+  password: string;
+  rememberMe: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.username = '';
+    this.password = '';
+    this.rememberMe = false;
   }
 
+  ngOnInit(): void {}
+
+  handleFormSubmit(event: Event) {
+    event.preventDefault();
+
+    const newData = {
+      username: this.username,
+      password: this.password,
+      rememberMe: this.rememberMe,
+    };
+
+    console.log('Login Data: ', newData);
+
+    this.username = '';
+    this.password = '';
+    this.rememberMe = false;
+  }
 }
