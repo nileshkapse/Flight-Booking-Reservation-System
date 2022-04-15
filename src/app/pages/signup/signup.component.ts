8b5552fbc883add6188ff34c55dc9ee9ec0e351b
Sanket Salunke <salunkess2001@gmail.com>
2022-04-15 11:00:25 +0530

    Signup form fields data accessed into .ts file and validations done

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -3,13 +3,48 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  email: string;
+  phoneNo: string;
+  password: string;
+  repeatPassword: string;
+  rememberMe: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.email = '';
+    this.phoneNo = '';
+    this.password = '';
+    this.repeatPassword = '';
+    this.rememberMe = false;
   }
 
+  ngOnInit(): void {}
+
+  handleFormSubmit(event: Event) {
+    event.preventDefault();
+
+    if (this.password !== this.repeatPassword) {
+      return console.log('Password not matched');
+    }
+
+    // Getting username from email
+    const username = this.email.split('@')[0];
+
+    const newData = {
+      username: username,
+      email: this.email,
+      phoneNo: this.phoneNo,
+      password: this.password,
+    };
+
+    console.log('User Signup Data: ', newData);
+
+    this.email = '';
+    this.phoneNo = '';
+    this.password = '';
+    this.repeatPassword = '';
+    this.rememberMe = false;
+  }
 }
