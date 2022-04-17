fb1cc3914f8c0392de91b3ae7615836d86f6778b
Sanket Salunke <salunkess2001@gmail.com>
2022-04-17 15:15:36 +0530

    Add New Flight form data accessed into .ts file

--- src/app/pages/add-flight/add-flight.component.ts
+++ src/app/pages/add-flight/add-flight.component.ts
@@ -3,13 +3,73 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-flight',
   templateUrl: './add-flight.component.html',
-  styleUrls: ['./add-flight.component.css']
+  styleUrls: ['./add-flight.component.css'],
 })
 export class AddFlightComponent implements OnInit {
+  flightName = '';
+  origin = '';
+  destination = '';
+  duration = '';
 
-  constructor() { }
+  departureDate = '';
+  departureTime = '';
+  returnDate = '';
+  returnTime = '';
 
-  ngOnInit(): void {
+  isEconomyClass = false;
+  economyClassSeats = '';
+  economyClassTicketPrice = '';
+
+  isBusinessClass = false;
+  businessClassSeats = '';
+  businessClassTicketPrice = '';
+
+  isFirstClass = false;
+  firstClassSeats = '';
+  firstClassTicketPrice = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  handleFormSubmit(event: Event) {
+    event.preventDefault();
+
+    var newFlightObject: any = {
+      flightName: this.flightName,
+      origin: this.origin,
+      destination: this.destination,
+      duration: this.duration,
+      departureDate: this.departureDate,
+      departureTime: this.departureTime,
+      returnDate: this.returnDate,
+      returnTime: this.returnTime,
+      isEconomyClass: this.isEconomyClass,
+      isBusinessClass: this.isBusinessClass,
+      isFirstClass: this.isFirstClass,
+    };
+
+    if (this.isEconomyClass) {
+      newFlightObject['economyClassSeats'] = parseInt(this.economyClassSeats);
+      newFlightObject['economyClassTicketPrice'] = parseInt(
+        this.economyClassTicketPrice
+      );
+    }
+
+    if (this.isBusinessClass) {
+      newFlightObject['businessClassSeats'] = parseInt(this.businessClassSeats);
+      newFlightObject['businessClassTicketPrice'] = parseInt(
+        this.businessClassTicketPrice
+      );
+    }
+
+    if (this.isFirstClass) {
+      newFlightObject['firstClassSeats'] = parseInt(this.firstClassSeats);
+      newFlightObject['firstClassTicketPrice'] = parseInt(
+        this.firstClassTicketPrice
+      );
+    }
+
+    console.log('New Flight Data: ', newFlightObject);
   }
-
 }
