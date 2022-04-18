16fbce63e05f729bb4a62c55a6843818b59f15fc
Sanket Salunke <salunkess2001@gmail.com>
2022-04-18 11:31:50 +0530

    Total seat counts displayed into details page

--- src/app/pages/flight-details/flight-details.component.ts
+++ src/app/pages/flight-details/flight-details.component.ts
@@ -8,6 +8,8 @@ import { FlightService } from 'src/app/services/Flight/flight.service';
 })
 export class FlightDetailsComponent implements OnInit {
   selectedFlight: any[];
+  totalSeats = 0;
+  remainingSeats = 0;
 
   constructor(private flightService: FlightService) {
     this.selectedFlight = [];
@@ -17,5 +19,37 @@ export class FlightDetailsComponent implements OnInit {
     this.flightService.getSelectedFlight().subscribe((flightData) => {
       this.selectedFlight = flightData;
     });
+  }
+
+  getTotalSeats() {
+    if (this.selectedFlight[0].isEconomyClass) {
+      this.totalSeats += this.selectedFlight[0].economyClassTotalSeats;
+    }
+
+    if (this.selectedFlight[0].isBusinessClass) {
+      this.totalSeats += this.selectedFlight[0].businessClassTotalSeats;
+    }
+
+    if (this.selectedFlight[0].isFirstClass) {
+      this.totalSeats += this.selectedFlight[0].firstClassTotalSeats;
+    }
+
+    return this.totalSeats;
+  }
+
+  getRemainingSeats() {
+    if (this.selectedFlight[0].isEconomyClass) {
+      this.remainingSeats += this.selectedFlight[0].economyClassRemainingSeats;
+    }
+
+    if (this.selectedFlight[0].isBusinessClass) {
+      this.remainingSeats += this.selectedFlight[0].businessClassRemainingSeats;
+    }
+
+    if (this.selectedFlight[0].isFirstClass) {
+      this.remainingSeats += this.selectedFlight[0].firstClassRemainingSeats;
+    }
+
+    return this.remainingSeats;
   }
 }
