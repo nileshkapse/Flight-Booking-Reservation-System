6ed6cb2adeca52e5b31b9a6cb4c2e11af94f87bb
Sanket Salunke <salunkess2001@gmail.com>
2022-04-22 14:31:35 +0530

    updated

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,15 +9,21 @@ import { UserService } from 'src/app/services/User/user.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  displayWelcomeHeader: boolean;
+
   imageSrc = 'assets/Images/menu2.png';
   user: any[];
   adminRole: any;
   constructor(private userService: UserService, private router: Router) {
     this.user = [];
     this.adminRole = ADMIN_ROLE;
+    this.displayWelcomeHeader = true;
   }
 
   ngOnInit(): void {
+    if (localStorage.getItem('displayWelcomeHeader')) {
+      this.displayWelcomeHeader = false;
+    }
     this.userService.getCurrentUser().subscribe((user) => {
       this.user = user;
     });
@@ -31,5 +37,10 @@ export class HeaderComponent implements OnInit {
     localStorage.clear();
     this.userService.logoutUser();
     this.router.navigate(['/login-page']);
+  }
+
+  handleHeaderRemove() {
+    this.displayWelcomeHeader = false;
+    localStorage.setItem('displayWelcomeHeader', 'false');
   }
 }
